a4ae1bee530f6fc5710a8384aa934c4594c2578f
Federico <federico.munafo@gmail.com>
2019-04-19 17:29:31 +0200

    Hello Sandbox_2

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 echo $1
+sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
+sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5
 
-if [ $1 == 'SandBox_1' ]; then
-	sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
-	sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5
-	exit 0
-fi
-
-if [ $1 == 'SandBox_2' ]; then
-    sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
-    sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5   
-   exit 0
-fi
