1d2e99e70cbc3d85535d3b4a44ce87221e8f6e30
Federico <federico.munafo@gmail.com>
2019-04-19 17:41:19 +0200

    Echo 1

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
 echo $1
-sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
-sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5
+
+sfdx force:org:list
+
+#sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
+#sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5
+
+#if [ $1 == 'SandBox_1' ]; then
+#	sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
+#	sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5
+#	exit 0
+#fi
+
+#if [ $1 == 'SandBox_2' ]; then
+#    sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
+#    sfdx force:mdapi:deploy -d mdapi_output/ -u HubOrg -w 5   
+#   exit 0
+#fi
 
