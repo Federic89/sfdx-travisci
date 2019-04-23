26c45957e28e7015909c59c007a8eeeb0f986bfc
Federico <federico.munafo@gmail.com>
2019-04-23 13:09:44 +0200

    Prova 8

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 echo $1
 
+if [ $1 == 'SandBox_1' ]; then
+    sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile assets/server.key --username $USERNAME --setdefaultdevhubusername -a SandBox1_Hub
+    exit 0
+fi
+if [ $1 == 'SandBox_1' ]; then
+    sfdx force:auth:jwt:grant --clientid $CONSUMERKEY_ORG2 --jwtkeyfile assets/server.key --username $USERNAME_ORG2 --setdefaultdevhubusername -a SandBox2_Hub
+    exit 0
+fi
+
 sfdx force:org:list
 
 #sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
