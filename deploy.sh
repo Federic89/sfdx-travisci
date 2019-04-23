db91adfa08d4b58304195e255077d2dee1902e5b
Federico <federico.munafo@gmail.com>
2019-04-23 13:19:02 +0200

    Prova deploy

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ if [ $1 == 'SandBox_1' ]; then
     sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile assets/server.key --username $USERNAME --setdefaultdevhubusername -a SandBox1_Hub
     sfdx force:org:list
     #deploy TODO
-    #sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
-    #sfdx force:mdapi:deploy -d mdapi_output/ -u SandBox1_Hub -w 5
+    sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
+    sfdx force:mdapi:deploy -d mdapi_output/ -u SandBox1_Hub -w 5
     exit 0
 fi
 if [ $1 == 'SandBox_2' ]; then
     sfdx force:auth:jwt:grant --clientid $CONSUMERKEY_ORG2 --jwtkeyfile assets/server.key --username $USERNAME_ORG2 --setdefaultdevhubusername -a SandBox2_Hub
     sfdx force:org:list
     #deploy TODO
-    #sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
-    #sfdx force:mdapi:deploy -d mdapi_output/ -u SandBox2_Hub -w 5
+    sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
+    sfdx force:mdapi:deploy -d mdapi_output/ -u SandBox2_Hub -w 5
     exit 0
 fi
 
