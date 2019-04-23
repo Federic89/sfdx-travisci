2a579023764e00daa7c6f8b0fe23e58d344affd5
Federico <federico.munafo@gmail.com>
2019-04-23 14:25:41 +0200

    Deploy prova ciao ciao 1001

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 echo $1
 
-if [ $1 == 'SandBox_1' ]; then
+if [ $1 == 'SandBox_1' ]
+then
     echo 'sono stronzo'
     sfdx force:auth:jwt:grant --clientid $CONSUMERKEY --jwtkeyfile assets/server.key --username $USERNAME --setdefaultdevhubusername -a SandBox1_Hub
     sfdx force:org:list
@@ -8,8 +9,8 @@ if [ $1 == 'SandBox_1' ]; then
     sfdx force:source:convert --outputdir mdapi_output_dir --packagename managed_pkg_name
     sfdx force:mdapi:deploy -d /home/travis/build/Federic89/sfdx-travisci/mdapi_output_dir/ -u SandBox1_Hub -w 5
     exit 0
-fi
-elif [ $1 == 'SandBox_2' ]; then
+elif [ $1 == 'SandBox_2' ]
+then
     sfdx force:auth:jwt:grant --clientid $CONSUMERKEY_ORG2 --jwtkeyfile assets/server.key --username $USERNAME_ORG2 --setdefaultdevhubusername -a SandBox2_Hub
     sfdx force:org:list
     #deploy TODO
